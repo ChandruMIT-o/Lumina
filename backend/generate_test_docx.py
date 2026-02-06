8206efd95b4e90df5bf075fff0ce0811553c36b7
Chandru J <chandrukavin0503@gmail.com>
2026-02-06 15:49:25 +0530

    deletable repo

--- backend/generate_test_docx.py
+++
@@ -1,8 +0,0 @@
-from docx import Document
-
-doc = Document()
-doc.add_heading('Lumina Test Document', 0)
-doc.add_paragraph('Hello, {{ name }}!')
-doc.add_paragraph('Today is {{ date }}.')
-doc.save('test_template.docx')
-print("Created test_template.docx")
